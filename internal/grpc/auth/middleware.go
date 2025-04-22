9a93cacd9a069578b6088cab1658c94e45a0f3e5
neandrson <a2361@yandex.ru>
2025-04-22 21:03:26 +0300

    Create middleware.go

---
+++ internal/grpc/auth/middleware.go
@@ -0,0 +1,47 @@
+package authgrpc
+
+import (
+	"context"
+
+	gojwt "github.com/golang-jwt/jwt/v5"
+	"github.com/neandrson/go_calc_final/internal/lib/jwt"
+	"github.com/neandrson/go_calc_final/internal/repositories/app"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+)
+
+func JWTMiddleware(appRepo app.Repository) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		md, ok := metadata.FromIncomingContext(ctx)
+		if !ok {
+			return nil, status.Errorf(codes.Unauthenticated, "metadata is not provided")
+		}
+
+		values := md["authorization"]
+		if len(values) == 0 {
+			return nil, status.Errorf(codes.Unauthenticated, "authorization token is not provided")
+		}
+
+		token := values[0]
+
+		// Проверка токена
+		// Здесь должен быть ваш код для проверки токена
+		// Например, вы можете использовать библиотеку для работы с JWT
+		// Если токен недействителен, верните ошибку
+		err, jwtToken := jwt.ProcessJWT(ctx, token, appRepo)
+		if err != nil {
+			return nil, status.Errorf(codes.Unauthenticated, "invalid token")
+		}
+		if claims, ok := jwtToken.Claims.(gojwt.MapClaims); ok {
+			userId, ok := claims["uid"].(float64)
+			if ok {
+				// Извлечение данных из токена
+				ctx = context.WithValue(ctx, "userID", userId)
+			}
+		}
+		// Если токен действителен, продолжайте обработку запроса
+		return handler(ctx, req)
+	}
+}
