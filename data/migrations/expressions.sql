6d14087e49581b8fdfc32426da4a2e3781b98b79
neandrson <a2361@yandex.ru>
2025-04-22 21:02:38 +0300

    Create expressions.sql

---
+++ data/migrations/expressions.sql
@@ -0,0 +1,13 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS expressions
+(
+    id UUID PRIMARY KEY,
+    user_id VARCHAR(255) NOT NULL,
+    idempotency_key VARCHAR(255) NOT NULL,
+    value TEXT NOT NULL,
+    state VARCHAR(50) NOT NULL,
+    result DOUBLE PRECISION,
+    created_at timestamp NOT NULL DEFAULT NOW(),
+    UNIQUE (user_id, idempotency_key)
+);
