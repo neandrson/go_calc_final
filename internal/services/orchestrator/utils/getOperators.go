f32091376453c4b8040604cf6d459aa196c129aa
neandrson <a2361@yandex.ru>
2025-04-22 21:04:37 +0300

    Create getOperators.go

---
+++ internal/services/orchestrator/utils/getOperators.go
@@ -0,0 +1,23 @@
+package orchestratorutils
+
+import (
+	"time"
+
+	"github.com/neandrson/go_calc_final/internal/config"
+	"github.com/neandrson/go_calc_final/internal/models"
+)
+
+// GetOperators возвращает список операция
+func GetOperators(timeouts config.CalculationTimeoutsConfig) []*models.Operator {
+	operatorsMap := map[string]time.Duration{
+		"+": timeouts.TimeCalculatePlus / time.Second,
+		"-": timeouts.TimeCalculateMinus / time.Second,
+		"*": timeouts.TimeCalculateMult / time.Second,
+		"/": timeouts.TimeCalculateDivide / time.Second,
+	}
+	var operators []*models.Operator
+	for key, value := range operatorsMap {
+		operators = append(operators, &models.Operator{Op: key, Timeout: value})
+	}
+	return operators
+}
